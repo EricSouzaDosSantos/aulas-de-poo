28f5c8e3b70a484904808446cb278983b2a9bca2
Eric <ericsouza94895@gmail.com>
2025-03-22 00:00:19 -0300

    feat: add class of exercise 1

---
+++ src/main/java/com/senai/estudos/poo/aula_06/abstracao/exercicios/check_in/LATAM.java
@@ -0,0 +1,16 @@
+package com.senai.estudos.poo.aula_06.abstracao.exercicios.check_in;
+
+class LATAM implements CheckIn {
+    @Override
+    public void validarDocumento(String documento) {
+        if (documento == null || documento.isEmpty()) {
+            throw new IllegalArgumentException("Documento inválido!");
+        }
+        System.out.println("Documento " + documento + " validado com sucesso.");
+    }
+
+    @Override
+    public void emitirCartaoEmbarque(String passageiro) {
+        System.out.println("Cartão de embarque emitido para: " + passageiro);
+    }
+}
