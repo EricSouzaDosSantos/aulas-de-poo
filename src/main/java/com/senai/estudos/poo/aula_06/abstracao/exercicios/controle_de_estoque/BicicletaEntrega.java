515d46f8bf685641e74f7044d42061aa8f2672ee
Eric <ericsouza94895@gmail.com>
2025-03-24 23:59:30 -0300

    feat: add class that represents the means of transport via bike

---
+++ src/main/java/com/senai/estudos/poo/aula_06/abstracao/exercicios/controle_de_estoque/BicicletaEntrega.java
@@ -0,0 +1,15 @@
+package com.senai.estudos.poo.aula_06.abstracao.exercicios.controle_de_estoque;
+
+public class BicicletaEntrega extends VeiculoEntrega {
+    private static final double VELOCIDADE_MAXIMA = 25.0; // Velocidade máxima permitida
+
+    public BicicletaEntrega(double velocidadeAtual, double capacidadeMaxima, double distanciaDestino) {
+        super(Math.min(velocidadeAtual, VELOCIDADE_MAXIMA), capacidadeMaxima, distanciaDestino);
+    }
+
+    @Override
+    public double calcularTempoEntrega() {
+        return distanciaDestino / velocidadeAtual;
+    }
+}
+
