1908ff9e15b59b29bde8c95d64b325f33658a8ee
Eric <ericsouza94895@gmail.com>
2025-03-26 17:13:19 -0300

    refactor: refactoring method that tests the methods of the Pagamento interface

--- src/main/java/com/senai/estudos/poo/aula_06/abstracao/exercicios/sistema_de_pagamentos/main/SistemaPagamento.java
+++ src/main/java/com/senai/estudos/poo/aula_06/abstracao/exercicios/sistema_de_pagamentos/main/SistemaPagamento.java
@@ -5,17 +5,19 @@ import com.senai.estudos.poo.aula_06.abstracao.exercicios.sistema_de_pagamentos.
 
 public class SistemaPagamento {
     public static void main(String[] args) {
-        Pagamento pagamento1 = new PagamentoCartaoCredito("João", 150.75);
-        pagamento1.processarPagamento();
-
-        System.out.println();
-
-        Pagamento pagamento2 = new PagamentoPIX("Maria", 500.00);
-        pagamento2.processarPagamento();
-
-        System.out.println();
-
-        Pagamento pagamento3 = new PagamentoCarteiraDigital("Carlos", 300.50);
-        pagamento3.processarPagamento();
+        Pagamento pagamentoCartaoCredito = new PagamentoCartaoCredito("João", 150.75);
+        testarPAgamento(pagamentoCartaoCredito);
+        
+        Pagamento pagamentoPIX = new PagamentoPIX("Maria", 500.00);
+        testarPAgamento(pagamentoPIX);
+        
+        Pagamento pagamentoCarteiraDigital = new PagamentoCarteiraDigital("Carlos", 300.50);
+        testarPAgamento(pagamentoCarteiraDigital);
+    }
+    
+    
+    private static void testarPAgamento(Pagamento pagamento){
+        System.out.println("----------------------------------");
+        pagamento.processarPagamento();
     }
 }
