25ea95ea3c71eb15fad035c846243c40c840af4f
Eric <ericsouza94895@gmail.com>
2025-03-26 17:13:08 -0300

    refactor: refactoring method that tests the methods of the Veiculo interface

--- src/main/java/com/senai/estudos/poo/aula_06/abstracao/exercicios/controle_de_estoque/main/Main.java
+++ src/main/java/com/senai/estudos/poo/aula_06/abstracao/exercicios/controle_de_estoque/main/Main.java
@@ -2,18 +2,26 @@ package com.senai.estudos.poo.aula_06.abstracao.exercicios.controle_de_estoque.m
 
 import com.senai.estudos.poo.aula_06.abstracao.exercicios.controle_de_estoque.BicicletaEntrega;
 import com.senai.estudos.poo.aula_06.abstracao.exercicios.controle_de_estoque.MotoEntrega;
+import com.senai.estudos.poo.aula_06.abstracao.exercicios.controle_de_estoque.VeiculoEntrega;
 
 public class Main {
     public static void main(String[] args) {
         MotoEntrega moto = new MotoEntrega(60, 20, 30);
+        testarVeiculo(moto);
+
         BicicletaEntrega bicicleta = new BicicletaEntrega(20, 10, 10);
+        testarVeiculo(bicicleta);
 
-        System.out.println("Detalhes da Moto de Entrega:");
-        moto.exibirDetalhes();
-        System.out.println();
+    }
 
-        System.out.println("Detalhes da Bicicleta de Entrega:");
-        bicicleta.exibirDetalhes();
+    private static void testarVeiculo(VeiculoEntrega veiculoEntrega){
+        System.out.println("--------------------------------------");
+        if (veiculoEntrega instanceof MotoEntrega){
+            System.out.println("Detalhes da Moto de Entrega:");
+        }else {
+            System.out.println("Detalhes da Bicicleta de Entrega:");
+        }
+        veiculoEntrega.exibirDetalhes();
     }
 }
 
